7ef16e947fb6ed95792797b219a3e38ab70beca1
elldebe <luca.di.benedetto@anyhelpnow.com>
2025-01-30 11:04:18 +0100

    Revert "live"
    This reverts commit 5765296eb0987095aedcd87af213cca4f0b92912, reversing
    changes made to aa73396fb49447eb23679f74b8996ad5d668d8eb.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import JSONGenerator from './JSONGenerator';  // Make sure this path matches your file structure
+
+function App() {
+  return (
+    <div className="App">
+      <JSONGenerator />
+    </div>
+  );
+}
+
+export default App;
